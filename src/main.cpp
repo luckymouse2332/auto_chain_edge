88c02222be735b98fcb32ad2f9833847296a5fab
luckymouse2332 <mousexiaoqi111@163.com>
2025-11-14 21:29:53 +0800

    最小可运行版本修正

--- src/main.cpp
+++ src/main.cpp
@@ -4,99 +4,94 @@
 
 #include "conveyor.h"
 
-Conveyor conveyor(12, 13, 11);
+Conveyor conveyor(13, 12, 11);
 MeArm arm;
 Servo pusher;
+
+String status;
 
 void setup()
 {
     Serial.begin(9600);
 
     conveyor.setup();
-    arm.begin(4, 5, 6, 7);
+    arm.begin(3, 5, 6, 9);
     pusher.attach(8);
+    delay(1000);
     pusher.write(30);
+    delay(1000);
 }
 
 void loop()
 {
-
-    static unsigned long lastTime1 = 0, lastTime2 = 0;
-    if (digitalRead(A1) != 1 && millis() - lastTime1 > 1000)
+    if (digitalRead(A1))
     {
-        Serial.println("S1");
-        lastTime1 = millis();
-    }
-
-    if (digitalRead(A0) != 1 && millis() - lastTime2 > 1000)
-    {
-        Serial.println("S2");
-        lastTime2 = millis();
-    }
-
-    String command;
-    if (Serial.available())
-    {
-        command = Serial.readStringUntil('\n');
-        Serial.println("OK"); // 回发
-
-        Serial.println("Got: " + command);
-    }
-
-    command.trim();            // 移除首尾空白
-    if (command.length() != 0) // 还没有信息
-    {
-        Serial.println("NoContent");
         return;
     }
+    delay(3000);
+    arm.snapToXYZ(-80, 0, 0);
+    arm.openClaw();
+    delay(1000);
+    arm.snapToXYZ(-140, 0, -20);
+    delay(1000);
+    arm.closeClaw();
+    delay(1000);
 
-    // 分割指令
-    char cmd_buffer[120];
-    command.toCharArray(cmd_buffer, 120);
-    char *token = strtok(cmd_buffer, ",");
-
-    if (token != NULL)
+    conveyor.stop();
+    arm.snapToXYZ(-80, 0, 20);
+    delay(1000);
+    arm.snapToXYZ(0, 90, 40);
+    delay(500);
+    arm.snapToXYZ(0, 120, 40);
+    delay(500);
+    arm.openClaw();
+    delay(1000);
+    arm.snapToXYZ(0, 100, 50);
+    delay(1000);
+    while (analogRead(A4) <= 512)
     {
-        if (String(token).equalsIgnoreCase("F"))
-        {
-            int pwm = atoi(strtok(NULL, ","));
-            conveyor.run(Direction::Forward, pwm);
-            Serial.println("Conveyor started!");
-        }
-        else if (String(token).equalsIgnoreCase("R"))
-        {
-            int pwm = atoi(strtok(NULL, ","));
-            conveyor.run(Direction::Reverse, pwm);
-            Serial.println("Conveyor started!");
-        }
-        else if (String(token).equalsIgnoreCase("IK"))
-        {
-            double x = atof(strtok(NULL, ","));
-            double y = atof(strtok(NULL, ","));
-            double z = atof(strtok(NULL, ","));
-            arm.moveToXYZ(x, y, z);
-            Serial.println("Arm has moved to the position!");
-        }
-        else if (String(token).equalsIgnoreCase("CL"))
-        {
-            String cmd = strtok(NULL, ",");
-            if (cmd.equalsIgnoreCase("Open"))
-            {
-                arm.openClaw();
-            }
-            else
-            {
-                arm.closeClaw();
-            }
-        }
-        else if (String(token).equalsIgnoreCase("P"))
-        {
-            int angle = atoi(strtok(NULL, ","));
-            pusher.write(angle);
-        }
-        else
-        {
-            Serial.println("Error: Unkown command!" + String(token));
-        }
+        delay(5);
     }
+    Serial.println("OK");
+    if (analogRead(A5) >= 512)
+    {
+        conveyor.run();
+        while (digitalRead(A0))
+        {
+            delay(5);
+        }
+        delay(1000);
+        conveyor.stop();
+        pusher.write(130);
+        delay(1000);
+        pusher.write(30);
+        delay(1000);
+        conveyor.run();
+    }
+    conveyor.run();
+    // while (true)
+    // {
+    //   status = Serial.readStringUntil('\n');
+    //   if (status != "") break;
+    // }
+    // Serial.println("Running");
+    // conveyor.run();
+    // for (size_t i = 0; i < status.length(); i++)
+    // {
+    //   if (status[i] == 'b')
+    //   {
+    //     while (digitalRead(A0))
+    //     {
+    //       delay(5);
+    //     }
+    //     delay(1000);
+    //     conveyor.stop();
+    //     pusher.write(130);
+    //     delay(1000);
+    //     pusher.write(30);
+    //     delay(1000);
+    //     conveyor.run();
+    //     break;
+    //   }
+    // }
 }
